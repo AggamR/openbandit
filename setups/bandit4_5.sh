fe242804fc81c1c082187cc54c708aa2026e8cd0
Aggam Rahamim <aggamrahamim@gmail.com>
2026-03-28 01:46:45 +0300

    realized isnt right for bandit4_5

--- setups/bandit4_5.sh
+++ setups/bandit4_5.sh
@@ -1,7 +1,8 @@
 mkdir /home/bandit4/inhere
 
 for i in {1..9}; do
-    echo $(generate_password) > "/home/bandit4/inhere/-file0$i"
+    (< /dev/urandom | head -c 16; echo) > "/home/bandit4/inhere/-file0$i"
 done
 
 echo "${passwords[5]}" > "/home/bandit4/inhere/-file07"
+
