d9af94a9122c737300ea602db1ac9f45cf2adf89
Aggam Rahamim <aggamrahamim@gmail.com>
2026-03-28 00:48:30 +0300

    bandit6_7 implemented

--- README.md
+++ README.md
@@ -10,6 +10,7 @@ meant to deploy on airgapped environments for training, or just for fun.
 - bandit3_4
 - bandit4_5
 - bandit5_6
+- bandit6_7
 
 ## Basic Explenation
 Each exercise has a different user and directory and so they're all set-up at build time in `setup.sh`,
---
+++ setups/bandit6_7.sh
@@ -0,0 +1,8 @@
+file_path=/var/lib/dpkg/info/bandit7.password
+mkdir -p /var/lib/dpkg/info 2&> /dev/null  # incase this isn't running on debian
+
+# file length = full length (33) - password_length
+password_length=${#passwords[7]}
+full_length=33  # variable for readbility, fuck magic numbers
+echo "${passwords[7]}$(printf " %*s" $((full_length - password_length - 2)))" > $file_path
+chown bandit7:bandit6 $file_path
